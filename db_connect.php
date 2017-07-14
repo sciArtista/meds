42e5cba79b29ddc68ebba19709244fa766d85d36
sciArtista <jafe_tech@live.com>
2017-07-14 17:58:13 +0300

    Create db_connect.php

---
+++ db_connect.php
@@ -0,0 +1,19 @@
+<?php header('Access-Control-Allow-Origin: *'); ?>
+<?php header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept'); ?>
+<?php header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT'); ?>
+<?php
+
+$db_host = "localhost"; 
+// Place the username for the MySQL database here 
+$db_username = "root"; 
+// Place the password for the MySQL database here 
+$db_pass = "";  
+// Place the name for the MySQL database here 
+$db_name = "medscrm";
+
+// Run the actual connection here  
+mysql_connect("$db_host","$db_username","$db_pass") or die ("could not connect to mysql");
+mysql_select_db("$db_name") or die ("no database");
+
+//echo "test";
+?>
